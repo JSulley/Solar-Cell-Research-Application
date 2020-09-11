74faf83cf22b770042f383d6236d30a8cae93bb6
JSulley <66404238+JSulley@users.noreply.github.com>
2020-09-11 14:58:26 -0500

    Update Absolute Max Function 2.0.R
    Optimized code slightly.

--- Absolute Max Function 2.0.R
+++ Absolute Max Function 2.0.R
@@ -1,50 +1,36 @@
 #Absolute Max Function
 abs_max_func_2 <- function(x, wave1, wave2) {
   
-  #Assign data frame as x and format it to a dataframe
-  x <- as.data.frame(x)
+  #Convert to matrix
+  x <- as.matrix(x)
   
-  #Subset the wavelengths as a vector by unlisting (Wavelengths are in the
-  #first row). 
-  dataset_wavelengths <- x[1,]
-  
-  #Get rid of the names associated with each vector element.
-  dataset_wavelengths <- unlist(dataset_wavelengths, use.names = FALSE)
-  
-  #Since we are subsetting the wavelengths, we need to get rid of the first row of x
-  #Note -c(1,2) will get rid of the first two elements because they are NA
-  dataset_wavelengths <- dataset_wavelengths[-c(1,2)]
-  x <- x[-1,]
-  
-  #Subset coordinates from x (first two columns)
-  dataset_coordinates <- x[,c(1,2)]
-  
-  #Subset x coordinates (first column of dataset_coordinates)
-  x_coordinate <- unlist(dataset_coordinates[,1], use.names = FALSE)
-  
-  #Subset y coordinates (second column of dataset_coordinates)
-  y_coordinate <- unlist(dataset_coordinates[,2], use.names = FALSE)
-  
-  #Omit first two columns from x
+  #Get rid of the coordinates from the data set
   x <- x[,-c(1,2)]
   
-  #Determine the dimensions of x. 
-  #The number of rows (first entry) indicate the number of iterations needed to extract the desired information
-  num_of_rows <- dim(x)[1]
+  #Subset the wavelengths as a vector by unlisting (Wavelengths are in the
+  #first row). Get rid of the names associated with each vector element.
+  #Since we are subsetting the wavelengths, we need to get rid of the first
+  #row in the big dataset
+  #Note -c(1,2) will get rid of the first two elements because they are 
+  #NA
+  dataset_wavelengths <- unlist(x[1,], use.names = FALSE)
+  x <- x[-1,]
   
-  #Make a list of data frames with the x,y coordinates and the wavelength
+  
+  #Make a list for the predicted wavelength 1 and 2, their respective normalized values, and relative intensity
   intensity_list <- list()
   
   #Make for loop
-  for (i in 1:num_of_rows){
+  #The number of rows will be the iterations needed to get the information we need.
+  for (i in 1:nrow(x)){
     
     #Subset the ith row and convert to a vector by unlisting along with
     #getting rid of the names associated with each element.
-    ith_row <- x[i,]
-    ith_row_vector <- unlist(ith_row, use.names = FALSE)
+    ith_row <- unlist(x[i,], use.names = FALSE)
     
-    #Make smooth spline with wavelengths as independent var. and the corresponding intensities as dependent var.
-    splinesmoothfit <- smooth.spline(x = dataset_wavelengths, y = ith_row_vector)
+    #Make smooth spline with wavelengths on the horizontal axis and the corresponding intensities
+    #on the vertical axis
+    splinesmoothfit <- smooth.spline(x = dataset_wavelengths, y = ith_row)
     
     #Use smooth spline again to predict the values for wave1 and wave2
     xyTabSmooth <- predict(splinesmoothfit, x = data.frame(wave1,wave2))
@@ -60,17 +46,13 @@ abs_max_func_2 <- function(x, wave1, wave2) {
     
   }
   
-  #Bind all the rows together and make object into dataframe
-  intensity_df <- rbindlist(intensity_list)
-  intensity_df <- as.data.frame(intensity_df)
+  #Convert list to dataframe
+  intensity_df <- as.data.frame(rbindlist(intensity_list))
   
   #Normalize intensities
-  max_int_wave1 <- max(intensity_df[,1])
-  max_int_wave2 <- max(intensity_df[,2])
-  
   #Divide every number in said column by maximum number
-  norm_wave1_int <- intensity_df[,1]/max_int_wave1
-  norm_wave2_int <- intensity_df[,2]/max_int_wave2
+  norm_wave1_int <- intensity_df[,1]/max(intensity_df[,1])
+  norm_wave2_int <- intensity_df[,2]/max(intensity_df[,2])
   
   #Combine normalized columns with the existing table and rearrange accordingly
   intensity_df <- cbind(intensity_df, norm_wave1_int, norm_wave2_int)
