a60ecef4ca7bff89c998d4100f1572428b04fef8
JSulley <66404238+JSulley@users.noreply.github.com>
2020-09-25 22:39:22 -0500

    Update Local Max Function.R
    Updated code to better determine the local max coordinates without inadvertently including the absolute max coordinates.

--- Local Max Function.R
+++ Local Max Function.R
@@ -7,6 +7,9 @@ local_max_func <- function(x) {
   #Extract Wavelengths
   wave <- z[1,-(1:2)]
   
+  #Make endpoints vector
+  endpoints_x <- c(min(wave), max(wave))
+  
   #Wavelengths and coordinates (1st 2 columns are not needed)
   z <- z[-1,-(1:2)]
   
@@ -26,7 +29,8 @@ local_max_func <- function(x) {
     #Create smooth spline based on values
     smooth <- smooth.spline(wave, intensity_norm)
     predicted <- predict(smooth, x = wave)
-    
+    endpoints_y <- unlist(predicted$y, use.names = FALSE)
+     
     #Solve for critical values where derivative equals to 0
     newsmooth <- SmoothSplineAsPiecePoly(smooth)
     xs <- solve(newsmooth, deriv = 1)
@@ -39,10 +43,10 @@ local_max_func <- function(x) {
     for (i in 1:length(xs)) {
       
       #If left side of critical value is increasing
-      if (predict(newsmooth,xs[i] - 1, deriv = 1) > 0) {
+      if (predict(newsmooth,xs[i] - 1e-8, deriv = 1) > 0) {
         
         #If right side of critical value is decreasing
-        if (predict(newsmooth,xs[i] + 1,deriv = 1) < 0) {
+        if (predict(newsmooth,xs[i] + 1e-8, deriv = 1) < 0) {
           
           #Assign value 1
           ext_val[i] <- 1
@@ -50,24 +54,44 @@ local_max_func <- function(x) {
       } 
     }
     
-    #Determine local max values from ys vector. 
-    #These values make up local_max_y
+    #Determine positions where value is 1
+    #Use this to determine local max coordinates
     local_max_y <- ys[which(ext_val == 1)]
     local_max_x <- xs[which(ext_val == 1)]
     
-    #Determine if absolute max is one of the local max values
-    if (max(predicted$y) %in% local_max_y) {
+    #Determine absolute max value
+    if (max(ys) > max(endpoints_y)) {
       
-      #Remove if true
-      local_max_y <- local_max_y[-which(local_max_y == max(predicted$y))]
-      local_max_x <- local_max_x[-which(local_max_y == max(predicted$y))]
+      #If local extrema contains the absolute max
+      abs_y <- max(ys)
+      abs_x <- xs[which.max(ys)]
+      
+    } else {
+      
+      #If either endpoints has absolute max
+      abs_y <- max(endpoints_y)
+      abs_x <- endpoints_x[which.max(endpoints_y)]
+      
     }
     
-    #Add vectors to respective list
+    #Determine if the absolute max is inside the local extrema values
+    if (abs_y %in% local_max_y) {
+      
+      #If it is in the local_max_y, get rid of it along with its corresponding x-value
+      local_max_y <- local_max_y[-which.max(local_max_y)]
+      local_max_x <- local_max_x[-which.max(local_max_y)]
+      
+    }
+    
+    #Add vectors to their respective list
     local_max_y_list[[j]] <- local_max_y 
     local_max_x_list[[j]] <- local_max_x
+    
   }
   
   #Merge both lists into one
   local_max_list <- c(local_max_x_list,local_max_y_list)
+  
+  #Display result
+  local_max_list
 }
